107b405147b128adb16c6c2e8b192d2195bfd050
soojik <wltn2858@swu.ac.kr>
2022-01-18 01:01:43 +0900

    upload 20220116

---
+++ programmers/phone_ketmon.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        
+        for (int i=0;i<nums.length-1;i++) {
+            for (int j=i+1;j<nums.length;j++) {
+                if (nums[i] == nums[j]) {
+                    nums[j] = -1;
+                }
+            }
+        }
+        
+        for (int i : nums) {
+            if (i > 0) {
+                answer++;
+            }
+        }
+        
+        answer = (answer>=nums.length/2) ? nums.length/2 : answer;
+
+        return answer;
+    }
+}
